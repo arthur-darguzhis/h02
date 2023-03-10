f31988cc5883d89fe0cbfdbf74971b9ed7c7584d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-10 16:40:20 +0300

    enable validation

--- src/auth/guards/basic.auth.guard.ts
+++ src/auth/guards/basic.auth.guard.ts
@@ -16,25 +16,21 @@ export class BasicAuthGuard implements CanActivate {
     const request: Request = context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization;
 
-    if (!request.headers.authorization) {
+    if (!authHeader || !authHeader.startsWith(BASIC_AUTH_PREFIX)) {
       throw new UnauthorizedException();
     }
 
-    // if (!authHeader || !authHeader.startsWith(BASIC_AUTH_PREFIX)) {
-    //   throw new UnauthorizedException();
-    // }
+    const base64Credentials = authHeader.slice(BASIC_AUTH_PREFIX.length);
+    const decodedCredentials = Buffer.from(
+      base64Credentials,
+      'base64',
+    ).toString();
+    const [login, password] = decodedCredentials.split(':');
 
-    // const base64Credentials = authHeader.slice(BASIC_AUTH_PREFIX.length);
-    // const decodedCredentials = Buffer.from(
-    //   base64Credentials,
-    //   'base64',
-    // ).toString();
-    // const [login, password] = decodedCredentials.split(':');
-    //
-    // const expectedCredentials = `${process.env.LOGIN_FOR_ADMIN_BASIC_AUTH}:${process.env.PASSWORD_FOR_ADMIN_BASIC_AUTH}`;
-    // if (`${login}:${password}` !== expectedCredentials) {
-    //   throw new UnauthorizedException();
-    // }
+    const expectedCredentials = `${process.env.LOGIN_FOR_ADMIN_BASIC_AUTH}:${process.env.PASSWORD_FOR_ADMIN_BASIC_AUTH}`;
+    if (`${login}:${password}` !== expectedCredentials) {
+      throw new UnauthorizedException();
+    }
     return true;
   }
 }
