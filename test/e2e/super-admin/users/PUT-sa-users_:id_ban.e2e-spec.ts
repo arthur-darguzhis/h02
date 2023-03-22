547eab7c838ea7eb7b8cee28bb2e9a8c1bb84a74
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-22 23:45:12 +0300

    add processing likes for comments

--- test/e2e/super-admin/users/PUT-sa-users_:id_ban.e2e-spec.ts
+++ test/e2e/super-admin/users/PUT-sa-users_:id_ban.e2e-spec.ts
@@ -3,7 +3,7 @@ import { setConfigTestApp } from '../../appConfigurations';
 import request from 'supertest';
 import { RequestsMaker } from '../../requestsMaker';
 
-//Очень сырой тест он не проверят забаненность коммента лайка по постам и по комментам
+//Очень сырой тест он не проверят забаненность коммента лайка по постам и по комментам и не проводти потом чтения.
 describe(`PUT /sa/users/:userId/ban (e2e)`, () => {
   let configuredTestApp: INestApplication;
   let app: any;
