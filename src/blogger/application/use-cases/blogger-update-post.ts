e75c2c3636f62e0e9c8d3ff6b62f0ad3856b1173
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-23 04:28:58 +0300

    add processing likes for comments

--- src/blogger/application/use-cases/blogger-update-post.ts
+++ src/blogger/application/use-cases/blogger-update-post.ts
@@ -2,6 +2,7 @@ import { CommandHandler } from '@nestjs/cqrs';
 import { PostsRepository } from '../../../posts/posts.repository';
 import { UnauthorizedActionException } from '../../../common/exceptions/domain.exceptions/unauthorized-action.exception';
 import { UpdatePostWithoutBlogIdDto } from '../../../posts/dto/updatePostWithoutBlogId.dto';
+import { BlogsRepository } from '../../../blogs/blogs.repository';
 
 export class BloggerUpdatePostCommand {
   constructor(
@@ -15,11 +16,18 @@ export class BloggerUpdatePostCommand {
 @CommandHandler(BloggerUpdatePostCommand)
 export class BloggerUpdatePostUseCase {
   constructor(
-    private postsRepository: PostsRepository, //TODO inject here necessary services
+    private postsRepository: PostsRepository,
+    private blogsRepository: BlogsRepository,
   ) {}
   async execute(command: BloggerUpdatePostCommand) {
-    const post = await this.postsRepository.getById(command.postId);
+    const blog = await this.blogsRepository.getById(command.blogId);
+    if (blog.blogOwnerInfo.userId !== command.userId) {
+      throw new UnauthorizedActionException(
+        'Unauthorized delete. This blog belongs to another user.',
+      );
+    }
 
+    const post = await this.postsRepository.getById(command.postId);
     if (post.postOwnerInfo.userId !== command.userId) {
       throw new UnauthorizedActionException(
         'Unauthorized update. This post belongs to another user.',
