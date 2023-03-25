5fe4850bc1f6b09c96f53920b42f845c097cda1f
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-25 19:43:25 +0300

    Ideal e2e test)

--- src/users/api/dto/createUser.dto.ts
+++ src/users/api/dto/createUser.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Length, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import { Trim } from '../../../common/crutches/class-transformer/trim.decorator';
 
 export class CreateUserDto {
@@ -6,15 +6,18 @@ export class CreateUserDto {
   @Length(3, 10)
   @Trim()
   @IsString()
+  @IsNotEmpty()
   login: string;
 
   @Length(6, 20)
   @Trim()
   @IsString()
+  @IsNotEmpty()
   password: string;
 
   @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) //@IsEmail()
   @Trim()
   @IsString()
+  @IsNotEmpty()
   email: string;
 }
