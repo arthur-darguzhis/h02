69b7a314e18ce47926fb0faeeba4fad667fcfd11
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-30 23:10:07 +0300

    Ideal e2e test)

--- src/blogger/api/dto/blogger-ban-user.dto.ts
+++ src/blogger/api/dto/blogger-ban-user.dto.ts
@@ -1,8 +1,15 @@
-import { IsIn, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsIn,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
 import { Trim } from '../../../common/crutches/class-transformer/trim.decorator';
 
 export class BloggerBanUserDto {
   @IsIn([true, false])
+  @IsBoolean()
   @IsNotEmpty()
   isBanned: boolean;
 
